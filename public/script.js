06bd201d5b819e4a370f83052f7538c7344c3ab6
Mateus Correia <mateuscorreia.dev@gmail.com>
2022-10-05 18:33:45 -0300

    added the recomendation products section and the function for the slider

--- public/script.js
+++ public/script.js
@@ -1,16 +1,35 @@
-// const imgs = document.getElementById("img");
-// const img = document.querySelectorAll("#img img");
+//This is about the carrossel function
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
 
-// let idx = 0;
+let idx = 0;
 
-// function carrossel() {
-//     idx++;
+function carrossel() {
+    idx++;
 
-//     if(idx > img.length - 1) {
-//         idx = 0;
-//     }
+    if(idx > img.length - 1) {
+        idx = 0;
+    }
 
-//     imgs.style.transform = `translateX(${-idx * 1900}px)`;
-// }
+    imgs.style.transform = `translateX(${-idx * 1900}px)`;
+}
 
-// setInterval(carrossel, 2000);
+setInterval(carrossel, 2000);
+
+//this is about the slider for the recomendation products
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimenstions = item.getBoundingClientRect();
+    let containerWidth = containerDimenstions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
